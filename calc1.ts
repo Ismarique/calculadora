e595bc3a43487802eebf86f8a5477e242fc2389d
ismael henrique <ismaelhenrique756@gmail.com>
2025-05-20 08:41:40 -0300

    termino da atividade

--- calc1.ts
+++ calc1.ts
@@ -13,16 +13,18 @@ function sair(): void{
 
 function opcoes (opcao:string): void{
     switch (opcao){
-        case '+': soma(); break;
-        case '-': subtracao(); break;
-        case '*': multiplicacao(); break;
-        case '/': divisao(); break;
-        case '0': sair(); break;
+        case '+': console.log(soma()); break;
+        case '-':  console.log(subtracao()); break;
+        case '*': console.log(multiplicacao()); break;
+        case '/': console.log(divisao()); break;
+        case '0': console.log(sair()); break;
         default: break;
     }    
 }
 
 function menu():void{
+    let teclado = require (`prompt-sync`)();
+    let opcao: string = ``;
     console.log(`---* CALCULADORA *---`);
     console.log(`Digite `);
     console.log(`+ --> ADIÇÃO`);
@@ -32,5 +34,44 @@ function menu():void{
     console.log(`0 --> SAIR`);
     opcao = teclado(`Digite a sua opção: `);
     opcoes(opcao);
+
 }
-menu(); 
+
+
+function soma (): number{
+    let teclado = require (`prompt-sync`)();
+    let n1: number = parseInt(teclado(`Digite o primeiro número: `));
+    let n2: number = parseInt(teclado(`Digite o segundo número: `));
+    let resultado: number = (n1 + n2);
+    return resultado;
+}
+
+function subtracao (): number{
+    let teclado = require (`prompt-sync`)();
+    let n1: number = parseInt(teclado(`Digite o primeiro número: `));
+    let n2: number = parseInt(teclado(`Digite o segundo número: `));
+    let resultado: number = (n1 - n2);
+    return resultado
+}
+
+function multiplicacao (): number{
+    let teclado = require (`prompt-sync`)();
+    let n1: number = parseInt(teclado(`Digite o primeiro número: `));
+    let n2: number = parseInt(teclado(`Digite o segundo número: `));
+    let resultado: number = (n1 * n2);
+    return resultado;
+}
+
+function divisao (): number{
+    let teclado = require (`prompt-sync`)();
+    let n1: number = parseInt(teclado(`Digite o primeiro número: `));
+    let n2: number = parseInt(teclado(`Digite o segundo número: `));
+    let resultado: number = (n1 / n2);
+    return resultado;
+}
+
+while(true){
+menu();
+
+}
+
